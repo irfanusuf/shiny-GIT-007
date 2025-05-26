a0b20fb4ffe42f42686de9acb8fe353c0309d7e3
irfan_usuf <irfanusuf33@gmail.com>
2025-05-26 16:11:23 +0530

    fixeDissues

--- P2WebMVC/obj/Debug/net8.0/P2WebMVC.AssemblyInfo.cs
+++ P2WebMVC/obj/Debug/net8.0/P2WebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P2WebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76264b25cce22205634b04015f942a5fd0417d10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1310c88adf8f96e98d9bf3f3edb4268342f7534")]
 [assembly: System.Reflection.AssemblyProductAttribute("P2WebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P2WebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
