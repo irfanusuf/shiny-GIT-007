8dfba4d755a6b21677f7ccf5309622947eee98fe
Irfan Yousuf <irfanusuf33@gmail.com>
2025-09-11 11:02:42 +0530

    fixes

--- P5_WebApi/Controllers/CartController.cs
+++ P5_WebApi/Controllers/CartController.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using P5_WebApi.Data;
 
 namespace P5_WebApi.Controllers
 {
@@ -7,5 +9,46 @@ namespace P5_WebApi.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
+
+
+        private readonly SqlDbContext sqlDb;
+        private readonly TokenService tokenService;
+        public CartController(SqlDbContext dbContext, TokenService tokenService)
+        {
+            sqlDb = dbContext;
+            this.tokenService = tokenService;
+
+        }
+
+
+        // [Authorize]
+        [HttpPost("addtocart")]
+        public ActionResult AddtoCart(Guid productId, string token)
+        {
+
+            try
+            {
+                var userId = tokenService.VerifyTokenAndGetId(token);
+
+                if (userId == Guid.Empty)
+                {
+                    return Unauthorized(new { message = "Unauthorized to access , kindly login again !" });
+                }
+
+
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+
+
+
     }
 }
